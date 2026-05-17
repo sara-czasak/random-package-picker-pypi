e56e5dba427c16350067e774036e653d273286ef
Star_Wasp7272 <star.wasp7272@gmail.com>
2026-05-17 11:31:21 +0200

    get random pypi package name and link

--- CLI_version/main.py
+++ CLI_version/main.py
@@ -9,3 +9,8 @@ soup = BeautifulSoup(r.text, 'html.parser')
 
 packages = soup.find_all('a')
 names = [i.text for i in packages]
+random_package = random.choice(names)
+random_package_link = f'https://pypi.org/project/{random_package}/'
+
+
+print(f"Random package: {random_package}\nLink: {random_package_link}")
